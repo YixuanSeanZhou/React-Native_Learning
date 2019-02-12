a81956993e09431c3546daf906abd287ea41083f
YixuanZhouThomas <132404780978@163.com>
2019-02-11 22:13:03 -0800

    The albums project

---
+++ albums/App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, {Component} from 'react';
+import {Text, AppRegistry} from 'react-native';
+
+// //const App = () => {
+//   //return (
+//     /  <Text>Some Text</Text>
+//     );
+// };
+
+export default class App extends Component{
+  render() {
+    return (
+      <Text> Some text </Text>
+      );
+  }
+}
+
+//AppRegistry.registerComponent('albums', () => App);
